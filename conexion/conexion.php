db5e70d1a4af34fbad4269dcd257023fc78f00ec
srpadilla <srpadilla@misena.edu.co>
2021-11-25 15:06:39 -0500

    primer commit antes de ir a github

---
+++ conexion/conexion.php
@@ -0,0 +1,32 @@
+<?php 
+class Conexion{    
+    private $dsn='mysql:host=sql437.main-hosting.eu;port=3306;dbname=u991668360_2142345';
+    private $usr='u991668360_adsi';
+    private $psw='Adsi2142345';  
+    private $cnx;
+public function __construct(){
+    try {
+        $this->cnx=new PDO($this->dsn,$this->usr,$this->psw); 
+        //echo 'clase padre <br>';
+        //print_r($this->cnx);
+        //echo '<br>';
+
+    } catch (PDOException $th) {
+        echo $th->getMessage();
+    }
+}
+
+public function desconectar(){
+    $this->cnx=null;
+}
+public function getCnx(){
+    return $this->cnx;
+}
+public function setCnx($cnx){
+    $this->cnx=$cnx;
+}
+}
+
+//$prueba=new Conexion();
+//echo $prueba->getCnx();
+?>
